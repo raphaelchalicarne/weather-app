eba9236d6894f292c22e190cdc8d49d515776f04
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-05-16 14:35:33 +1000

    Display background image

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,56 @@ var api_key = config.API_KEY;
 
 async function getWeather(city_name) {
     let promise_weather = fetch('https://api.openweathermap.org/data/2.5/find?q=' + city_name + '&units=metric&appid=' + api_key)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+            return response.json();
+        })
         .then(data => data.list[0])
         .then(function (weather_data) {
             console.log(weather_data);
+            
+            // Get proper name
             let city_name = weather_data.name;
+            let re = /^Arrondissement de/;
+            let array_test = city_name.split(re);
+            city_name = array_test[array_test.length -1];
+
             let country_emoji = countries[weather_data.sys.country].emoji;
             $('#city_name_flag').text(city_name + ' ' + country_emoji);
             let temp = weather_data.main.temp;
             $('#temp_sun').text(temp + '°C');
             let pressure = weather_data.main.pressure;
-            $('#pressure').text(pressure + ' hPa');
+            $('#pressure').text('Pressure : ' + pressure + ' hPa');
+            getPicture(city_name);
             $('#map').show();
-        });
+        })
+        .catch(error => console.log('The city was not found'));
     await promise_weather;
 }
 
 async function getPicture(city_name) {
-    city_name = city_name.toLowerCase();
+    city_name = city_name.toLowerCase().trim();
     let promise_pic = fetch('https://api.teleport.org/api/urban_areas/slug:' + city_name + '/images/')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+            return response.json();
+        })
         .then(function (data) {
             console.log(data);
             let mobile_url = data.photos[0].image.mobile;
             $('html').attr('style','background:url(' + mobile_url + ') no-repeat center center fixed; background-size:cover;');
+        })
+        .catch(error => {
+            $('html').attr('style','');
         });
     await promise_pic;
 }
 
 $('#get_weather').click(async function () {
     let city_name = $('#city_input').val();
-    await Promise.all([getWeather(city_name), getPicture(city_name)]);
-    // await getWeather(city_name);
-    // await getPicture(city_name);
+    await getWeather(city_name);
 });
