329388a51c2e7a91fb72dcc6fe7f008793a5e373
raphaelchalicarne <raphael.chalicarne@outlook.com>
2021-07-17 15:06:19 +0200

    Use a new API to fetch city pictures Fixes #1

--- js/modules/picture.js
+++ js/modules/picture.js
@@ -1,12 +1,11 @@
 import { config } from "../const/config.js";
 
 export async function getPicture(city_name) {
-    let headers = new Headers({'Authorization': 'Basic ' + config.PICTURE_AUTH});
-    // headers.set('Authorization', 'Basic ' + config.PICTURE_AUTH);
+    let headers = new Headers({ 'Authorization': 'Basic ' + config.PICTURE_AUTH });
 
     city_name = city_name.toLowerCase().trim();
     let query_url = 'https://api.roadgoat.com/api/v2/destinations/auto_complete?q=' + city_name;
-    let promise_pic = fetch(query_url, headers)
+    let promise_pic = fetch(query_url, { headers: headers })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -14,9 +13,8 @@ export async function getPicture(city_name) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            let mobile_url = data.photos[0].image.mobile;
-            $('html').attr('style', 'background:url(' + mobile_url + ') no-repeat center center fixed; background-size:cover;');
+            let photo_url = data.included[0].attributes.image.full;
+            $('html').attr('style', 'background:url(' + photo_url + ') no-repeat center center fixed; background-size:cover;');
         })
         .catch(error => {
             $('html').attr('style', '');
